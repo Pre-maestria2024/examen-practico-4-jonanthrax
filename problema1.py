f66247444b562504dc64b71e89d8eb6dc115ee8c
j0n4nthr4x <j2velasquezq@gmail.com>
2025-03-09 17:14:04 -0500

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -1,13 +1,12 @@
-inport pandas
 
 def main():
 
 	m, n = map(int, input().split())
 	H = list(map(int, input().split()))
 	D = list(map(int, input().split()))
-
-	
+	pass
 if __name__ == '__main__':
 	main()
-
-
+	print(1)
+	pass
+	
