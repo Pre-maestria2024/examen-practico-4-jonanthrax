e54d11da224a7151912a2267d673447010491df8
j0n4nthr4x <j2velasquezq@gmail.com>
2025-03-10 10:35:33 -0500

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -4,16 +4,14 @@ from queue import Queue
 def main():
 
     n, k = map(int, input().split())
-    
+    edges = []
     for i in range(n-1):
         u,v = map(int,input().split())
+        edges.append((u,v),)
         pass
     
     print("n:{} | k: {}".format(n,k))
-    print("u")
-    print(u)
-    print("=="*35)
-    print(v)
+    print("edges: ", edges)
     pass
 
 if __name__  == '__main__':
