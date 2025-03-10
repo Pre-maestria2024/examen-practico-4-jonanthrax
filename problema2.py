0cc480588a856adcaf671410e5c61d026c8692cc
j0n4nthr4x <j2velasquezq@gmail.com>
2025-03-10 10:27:30 -0500

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -7,6 +7,15 @@ def main():
     
     for i in range(n-1):
         u,v = map(int,input().split())
+        pass
+    
+    print("n:{} | k: {}".forma(n,k))
+    print("u")
+    print(u)
+    print("=="*35)
+    print(v)
+    pass
 
 if __name__  == '__main__':
     main()
+    pass
